c975be28efb344375863e1af668af997201108a0
Jiaming-Wang <wjmecho@163.com>
2020-11-11 20:40:22 +0800

    update

--- model/pannet.py
+++ model/pannet.py
@@ -3,8 +3,9 @@
 '''
 Author: wjm
 Date: 2020-11-05 20:47:04
-LastEditTime: 2020-11-11 16:29:08
-Description: PanNet: A deep network architecture for pan-sharpening
+LastEditTime: 2020-11-11 20:40:00
+Description: PanNet: A deep network architecture for pan-sharpening (VDSR-based)
+1e6, batch_size = 128, learning_rate = 1e-4, patch_size = 33
 '''
 import os
 import torch
@@ -18,19 +19,39 @@ class Net(nn.Module):
     def __init__(self, num_channels, base_filter, args):
         super(Net, self).__init__()
 
-        self.trans = TransConvBlock(4, 4, kernel_size=8, stride=4, padding=1)
+        base_filter = 64
+        num_channels = 7
+        out_channels = 4
+        self.head = ConvBlock(num_channels, 48, 9, 1, 4, activation='relu', norm=None, bias = True)
 
-        base_filter = 32
-        body = [
-            ResnetBlock(base_filter, 3, 1, 1, 0.1, activation='relu', norm=None) for _ in range(n_resblocks)
-        ]
-        self.body = nn.Sequential(*body)
+        self.body = ConvBlock(48, 32, 5, 1, 2, activation='relu', norm=None, bias = True)
 
-        self.output_conv = ConvBlock(base_filter, 4, 3, 1, 1, activation='relu', norm=None)
-    
-    def forward(self, x_ms, x_pan):
-        x_f = torch.cat((x_ms_up, x_pan), 1)
+        self.output_conv = ConvBlock(32, out_channels, 5, 1, 2, activation='relu', norm=None, bias = True)
+
+        for m in self.modules():
+            classname = m.__class__.__name__
+            if classname.find('Conv2d') != -1:
+        	    # torch.nn.init.kaiming_normal_(m.weight)
+        	    torch.nn.init.xavier_uniform_(m.weight, gain=1)
+        	    if m.bias is not None:
+        		    m.bias.data.zero_()
+            elif classname.find('ConvTranspose2d') != -1:
+        	    # torch.nn.init.kaiming_normal_(m.weight)
+        	    torch.nn.init.xavier_uniform_(m.weight, gain=1)
+        	    if m.bias is not None:
+        		    m.bias.data.zero_()
+
+    def forward(self, l_ms, b_ms, x_pan):
+
+        NDWI = ((l_ms[:, 1, :, :] - l_ms[:, 3, :, :]) / (l_ms[:, 1, :, :] + l_ms[:, 3, :, :])).unsqueeze(1)
+        NDWI = F.interpolate(NDWI, scale_factor=4, mode='bicubic')
+        NDVI = ((l_ms[:, 3, :, :] - l_ms[:, 2, :, :]) / (l_ms[:, 3, :, :] + l_ms[:, 2, :, :])).unsqueeze(1)
+        NDVI = F.interpolate(NDVI, scale_factor=4, mode='bicubic')
+        x_f = torch.cat([b_ms, x_pan, NDVI, NDWI], 1)
+        x_f = self.head(x_f)
         x_f = self.body(x_f)
         x_f = self.output_conv(x_f)
+        x_f = x_f + b_ms
+        
         return x_f
         
